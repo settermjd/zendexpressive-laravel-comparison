c2ce095753f15d8958407f0c6b4b8104e1358860
Matthew Setter <matthew@maltblue.com>
2016-07-27 15:57:52 +0200

    Allow created and updated columns to be nullable

--- migrations/Version20160718060344.php
+++ migrations/Version20160718060344.php
@@ -13,8 +13,8 @@ class Version20160718060344 extends AbstractMigration
         $table->addColumn("id", "integer");
         $table->addColumn("shortened_url", "text", ["notnull" => true]);
         $table->addColumn("original_url", "text", ["notnull" => true]);
-        $table->addColumn("created", "date", ["notnull" => true]);
-        $table->addColumn("updated", "date", ["notnull" => true]);
+        $table->addColumn("created", "date", ["notnull" => false]);
+        $table->addColumn("updated", "date", ["notnull" => false]);
 
         $table->setPrimaryKey(["id"]);
     }
